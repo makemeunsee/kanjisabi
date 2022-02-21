bce687e855658eca653d9f998b49076c84d07c88
makemeunsee <makemeunsee@gmail.com>
2022-02-21 01:54:25 +0100

    global hotkey closes window

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,41 @@
-use device_query::{DeviceQuery, DeviceState, MousePosition};
-use hotkey;
-use std::sync::Arc;
-use std::{thread, time};
-use tokio::sync::RwLock;
-use tokio::task;
+use device_query::{DeviceQuery, DeviceState};
+use tauri_hotkey::{Hotkey, HotkeyManager, Key};
 
 use winit::{
     dpi::{PhysicalPosition, PhysicalSize},
-    event::{DeviceEvent, ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent},
+    event::{DeviceEvent, Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
-    monitor::MonitorHandle,
-    platform::unix::WindowBuilderExtUnix,
-    window::{CursorIcon, Fullscreen, WindowBuilder},
+    window::WindowBuilder,
 };
 
-#[tokio::main]
-async fn main() {
+fn main() {
     let device_state = DeviceState::new();
     let mut mouse_pos = device_state.get_mouse().coords;
 
     let event_loop = EventLoop::new();
+    let proxy = event_loop.create_proxy();
+
+    let mut hkm = HotkeyManager::new();
+    match hkm.register(
+        Hotkey {
+            modifiers: vec![],
+            keys: vec![Key::ESCAPE],
+        },
+        move || {
+            proxy.send_event(()).unwrap();
+        },
+    ) {
+        Ok(_) => println!("hotkey registration Ok"),
+        Err(str) => println!("hotkey registration failed: {0}", str),
+    }
+
     let window = WindowBuilder::new()
         .with_maximized(true)
         .with_decorations(false)
         .with_transparent(true)
         .with_resizable(false)
         .with_always_on_top(true)
-        .with_visible(false)
+        // .with_visible(false)
         .with_inner_size(PhysicalSize::new(30 as i32, 30 as i32))
         // .with_fullscreen(Some(Fullscreen::Borderless(None)))
         // .with_override_redirect(true)
@@ -38,32 +47,16 @@ async fn main() {
     // window.set_cursor_icon(PREFERRED_CURSOR);
     // let mut cursor_idx = CURSORS.iter().position(|&c| PREFERRED_CURSOR == c).unwrap();
 
-    // let mut hk = hotkey::Listener::new();
-    // hk.register_hotkey(
-    //     hotkey::modifiers::CONTROL | hotkey::modifiers::SHIFT,
-    //     'K' as u32,
-    //     || {
-    //         window.set_visible(true);
-    //     },
-    // )
-    // .unwrap();
-
-    // hk.listen();
-
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
 
         match event {
+            Event::UserEvent(_) => {
+                println!("user event, stopping");
+                *control_flow = ControlFlow::Exit;
+            },
             Event::WindowEvent { event, window_id } /*if window.id() == window_id*/ => match event {
                 WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
-                WindowEvent::KeyboardInput {
-                    input:
-                        KeyboardInput {
-                            virtual_keycode: Some(VirtualKeyCode::Escape),
-                            ..
-                        },
-                    ..
-                } => *control_flow = ControlFlow::Exit,
                 // WindowEvent::KeyboardInput {
                 //     input:
                 //         KeyboardInput {
@@ -83,19 +76,12 @@ async fn main() {
                 // }
                 _ => (),
             },
-            // Event::RedrawRequested(window_id) if window.id() == window_id => {}
-            // Event::MainEventsCleared => {
-            //     window.request_redraw();
-            // },
             Event::DeviceEvent { event, .. } => match event {
                 DeviceEvent::MouseMotion { .. } => {
                     let pos = device_state.get_mouse().coords;
                     if mouse_pos != pos {
                         mouse_pos = pos;
-                        // println!("Current Mouse Coordinates: {:?}", pos);
                         window.set_outer_position(PhysicalPosition::new(pos.0, pos.1));
-                        // window.focus_window();
-                        // window.request_user_attention(Option::None);
                     }
                 }
                 _ => (),
