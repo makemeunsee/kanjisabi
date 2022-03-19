35a093124045f08fdd1087ce05adba16f1b17717
makemeunsee <makemeunsee@gmail.com>
2022-03-19 13:38:20 +0100

    drawing on a hidden window is usually a NOOP

--- src/main.rs
+++ src/main.rs
@@ -360,8 +360,8 @@ impl App {
                     self.reset_ocr();
                 } else {
                     // mouse hasn't moved
-                    self.capture_area.present();
                     self.capture_area.window_mut().show();
+                    self.capture_area.present();
                     self.elapsed_ticks_since_mouse_moved += 1;
 
                     if adjust_capture_area(
