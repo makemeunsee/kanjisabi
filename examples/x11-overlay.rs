e5c6f584cf701cc20487421f31cdfda0fc7fb090
makemeunsee <makemeunsee@gmail.com>
2022-03-20 04:05:51 +0100

    valid always on top x11 code
    not working with Xmonad though

--- examples/x11-overlay.rs
+++ examples/x11-overlay.rs
@@ -1,13 +1,12 @@
+use sdl2::sys::{MapNotify, SubstructureNotifyMask, SubstructureRedirectMask};
 use x11rb::connection::Connection;
-use x11rb::protocol::shape;
 use x11rb::protocol::xfixes::ConnectionExt as _;
 use x11rb::protocol::xfixes::{destroy_region, RegionWrapper, SetWindowShapeRegionRequest};
-use x11rb::protocol::xproto::ConnectionExt as _;
+use x11rb::protocol::xproto::{ClientMessageEvent, ConnectionExt as _, PropMode};
 use x11rb::protocol::xproto::{ColormapAlloc, ColormapWrapper, CreateWindowAux, WindowClass};
+use x11rb::protocol::{shape, Event};
+use x11rb::wrapper::ConnectionExt as _;
 
-// for always on top, see
-// https://docs.rs/x11rb/0.9.0/x11rb/protocol/xproto/fn.send_event.html
-// https://stackoverflow.com/questions/4345224/x11-xlib-window-always-on-top
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let (conn, screen_num) = x11rb::connect(None).unwrap();
 
@@ -46,9 +45,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             .background_pixel(0x30FF0000)
             .colormap(Some(cw.into_colormap()))
             .override_redirect(Some(1))
-            .border_pixel(Some(1)),
+            .border_pixel(Some(1))
+            .event_mask(0xFFFFFF),
     )?;
 
+    // input passthrough start
     let rw = RegionWrapper::create_region(&conn, &[]).unwrap();
 
     let set_shape_request = SetWindowShapeRegionRequest {
@@ -69,11 +70,73 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     };
     let _ = set_shape_request.send(&conn).unwrap();
     let _ = destroy_region(&conn, rw.region()).unwrap();
+    // input passthrough end
+
+    // always on top start
+    let wm_state = conn
+        .intern_atom(true, "_NET_WM_STATE".as_bytes())
+        .unwrap()
+        .reply()
+        .unwrap()
+        .atom;
+    let wm_state_above = conn
+        .intern_atom(true, "_NET_WM_STATE_FULLSCREEN".as_bytes())
+        .unwrap()
+        .reply()
+        .unwrap()
+        .atom;
+
+    println!("{} - {}", wm_state, wm_state_above);
+
+    // always on top - impl1
+    const _NET_WM_STATE_ADD: u32 = 1;
+    let event_always_on_top = ClientMessageEvent::new(
+        32,
+        win_id,
+        wm_state,
+        [_NET_WM_STATE_ADD, wm_state_above, 0, 0, 0],
+    );
+    // `event_always_on_top` sent in event loop
+
+    // // always on top - impl2
+    // const XA_ATOM: u32 = 4;
+    // let _ = conn
+    //     .change_property32(
+    //         PropMode::REPLACE,
+    //         win_id,
+    //         wm_state,
+    //         XA_ATOM,
+    //         &[wm_state_above],
+    //     )
+    //     .unwrap();
+
+    // always on top end
 
     conn.map_window(win_id)?;
-    conn.flush();
+
+    let _ = conn.flush();
+
+    let mut always_on_top_sent = false;
 
     loop {
-        println!("Event: {:?}", conn.wait_for_event()?);
+        let event = conn.wait_for_event()?;
+        println!("Event: {:?}", event);
+        if !always_on_top_sent {
+            match event {
+                Event::MapNotify(_) => {
+                    let _ = conn
+                        .send_event(
+                            false,
+                            screen.root,
+                            SubstructureRedirectMask | SubstructureNotifyMask,
+                            event_always_on_top,
+                        )
+                        .unwrap();
+                    always_on_top_sent = true;
+                    println!("kindly asked 'always on top'");
+                }
+                _ => (),
+            }
+        }
     }
 }
