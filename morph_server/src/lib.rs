8db506e73fb7b248595971828ce8e5f7449cbe18
makemeunsee <makemeunsee@gmail.com>
2022-10-08 19:07:17 +0200

    categorization + tests

--- morph_server/src/lib.rs
+++ morph_server/src/lib.rs
@@ -18,11 +18,13 @@ pub enum LexicalCategory {
     AdverbificationParticle,
     AdjectivisationParticle,
     ContinuativeParticle,
+    ContinuativeAuxiliary,
     Verb,
     AuxiliaryVerb,
     AttributiveParticle,
     Particle,
     CaseParticle,
+    ConnectingParticle,
     Sign,
 }
 
@@ -127,6 +129,7 @@ pub fn categorize(details: &Vec<String>) -> Option<LexicalCategory> {
 //TODO split into category and sub-category? +dict form morpheme
 #[cfg(feature = "unidic")]
 pub fn categorize(details: &Vec<String>) -> Option<LexicalCategory> {
+    //TODO remove
     println!("{:?}", details);
     match details
         .iter()
@@ -149,16 +152,19 @@ pub fn categorize(details: &Vec<String>) -> Option<LexicalCategory> {
             Some(LexicalCategory::AdverbificationParticle)
         }
         ["助動詞", _, _, _, _, "連用形-一般", _, _, _, _, _, _, _, _, _, _, _] => {
-            Some(LexicalCategory::ContinuativeParticle)
-        }
-        ["助詞", "接続助詞", _, _, _, _, _, _, "て", _, _, _, _, _, _, _, _] => {
-            Some(LexicalCategory::ContinuativeParticle)
+            Some(LexicalCategory::ContinuativeAuxiliary)
         }
         ["助動詞", _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _] => {
             Some(LexicalCategory::AuxiliaryVerb)
         }
+        ["助詞", "接続助詞", _, _, _, _, _, _, "て", _, _, _, _, _, _, _, _] => {
+            Some(LexicalCategory::ContinuativeParticle)
+        }
         ["助詞", "格助詞", _, _, _, _, _, _, _, _, _, _, _, _, _, _, _] => {
             Some(LexicalCategory::CaseParticle)
+        }
+        ["助詞", "係助詞", _, _, _, _, _, _, _, _, _, _, _, _, _, _, _] => {
+            Some(LexicalCategory::ConnectingParticle)
         }
         ["形容詞", "非自立可能", _, _, _, "終止形-一般", _, "無い", _, _, _, _, _, _, _, _, _] => {
             Some(LexicalCategory::NegationDictForm)
@@ -178,36 +184,9 @@ pub fn categorize(details: &Vec<String>) -> Option<LexicalCategory> {
         ["形容詞", _, _, _, _, "連用形-促音便", _, _, _, _, _, _, _, _, _, _, _] => {
             Some(LexicalCategory::AdjectiveIConjForm)
         }
+        ["副詞", _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _] => Some(LexicalCategory::Adverb),
         _ => None,
     }
-}
-
-// 簡単じゃない
-// 簡単ではない
-
-#[tokio::test]
-async fn 大抵_is_adverb() {
-    let details = tokenize("大抵").await.unwrap();
-    assert_eq!(details.len(), 1);
-    assert_eq!(categorize(&details[0]), Some(LexicalCategory::Adverb));
-}
-
-#[tokio::test]
-async fn あまり_is_adverb() {
-    let details = tokenize("あまり").await.unwrap();
-    assert_eq!(details.len(), 1);
-    assert_eq!(categorize(&details[0]), Some(LexicalCategory::Adverb));
-}
-
-#[tokio::test]
-async fn たかくない() {
-    let details = tokenize("たかくない").await.unwrap();
-    assert_eq!(details.len(), 2);
-    assert_eq!(categorize(&details[0]), Some(LexicalCategory::Adverb));
-    assert_eq!(
-        categorize(&details[1]),
-        Some(LexicalCategory::AuxiliaryVerb)
-    );
 }
 
 #[tokio::test]
@@ -257,7 +236,7 @@ async fn 降りました_is_verb() {
     assert_eq!(categorize(&details[0]), Some(LexicalCategory::Verb));
     assert_eq!(
         categorize(&details[1]),
-        Some(LexicalCategory::AuxiliaryVerb)
+        Some(LexicalCategory::ContinuativeAuxiliary)
     );
     assert_eq!(
         categorize(&details[2]),
@@ -280,18 +259,122 @@ async fn 降らない_is_verb() {
 async fn ために() {
     let details = tokenize("ために").await.unwrap();
     assert_eq!(details.len(), 2);
-    assert_eq!(categorize(&details[0]), None);
-    assert_eq!(categorize(&details[1]), None);
+    assert_eq!(categorize(&details[0]), Some(LexicalCategory::Noun));
+    assert_eq!(categorize(&details[1]), Some(LexicalCategory::CaseParticle));
 }
 
 #[tokio::test]
 async fn ため() {
     let details = tokenize("ため").await.unwrap();
     assert_eq!(details.len(), 1);
-    assert_eq!(categorize(&details[0]), None);
+    assert_eq!(categorize(&details[0]), Some(LexicalCategory::Noun));
 }
 
-// -----------------------------------------------
+#[tokio::test]
+async fn たかくない() {
+    let details = tokenize("たかくない").await.unwrap();
+    assert_eq!(details.len(), 2);
+    assert_eq!(
+        categorize(&details[0]),
+        Some(LexicalCategory::AdjectiveIConjForm)
+    );
+    assert_eq!(
+        categorize(&details[1]),
+        Some(LexicalCategory::NegationDictForm)
+    );
+}
+
+#[tokio::test]
+async fn 大抵_is_adverb() {
+    let details = tokenize("大抵").await.unwrap();
+    assert_eq!(details.len(), 1);
+    assert_eq!(categorize(&details[0]), Some(LexicalCategory::Adverb));
+}
+
+#[tokio::test]
+async fn あまり_is_adverb() {
+    let details = tokenize("あまり").await.unwrap();
+    assert_eq!(details.len(), 1);
+    assert_eq!(categorize(&details[0]), Some(LexicalCategory::Adverb));
+}
+
+#[tokio::test]
+async fn 簡単じゃありません() {
+    let details = tokenize("簡単じゃありません").await.unwrap();
+    assert_eq!(details.len(), 5);
+    assert_eq!(categorize(&details[0]), Some(LexicalCategory::Keiyoudoushi));
+    assert_eq!(
+        categorize(&details[1]),
+        Some(LexicalCategory::AuxiliaryVerb)
+    );
+    assert_eq!(categorize(&details[2]), Some(LexicalCategory::Verb));
+    assert_eq!(
+        categorize(&details[3]),
+        Some(LexicalCategory::AuxiliaryVerb)
+    );
+    assert_eq!(
+        categorize(&details[4]),
+        Some(LexicalCategory::AuxiliaryVerb)
+    );
+}
+
+#[tokio::test]
+async fn 簡単ではありません() {
+    let details = tokenize("簡単ではありません").await.unwrap();
+    assert_eq!(details.len(), 6);
+    assert_eq!(categorize(&details[0]), Some(LexicalCategory::Keiyoudoushi));
+    assert_eq!(
+        categorize(&details[1]),
+        Some(LexicalCategory::ContinuativeAuxiliary)
+    );
+    assert_eq!(
+        categorize(&details[2]),
+        Some(LexicalCategory::ConnectingParticle)
+    );
+    assert_eq!(categorize(&details[3]), Some(LexicalCategory::Verb));
+    assert_eq!(
+        categorize(&details[4]),
+        Some(LexicalCategory::AuxiliaryVerb)
+    );
+    assert_eq!(
+        categorize(&details[5]),
+        Some(LexicalCategory::AuxiliaryVerb)
+    );
+}
+
+#[tokio::test]
+async fn 簡単ではない() {
+    let details = tokenize("簡単ではない").await.unwrap();
+    assert_eq!(details.len(), 4);
+    assert_eq!(categorize(&details[0]), Some(LexicalCategory::Keiyoudoushi));
+    assert_eq!(
+        categorize(&details[1]),
+        Some(LexicalCategory::ContinuativeAuxiliary)
+    );
+    assert_eq!(
+        categorize(&details[2]),
+        Some(LexicalCategory::ConnectingParticle)
+    );
+    assert_eq!(
+        categorize(&details[3]),
+        Some(LexicalCategory::NegationDictForm)
+    );
+}
+
+#[tokio::test]
+async fn 簡単じゃない() {
+    let details = tokenize("簡単じゃない").await.unwrap();
+    assert_eq!(details.len(), 3);
+    assert_eq!(categorize(&details[0]), Some(LexicalCategory::Keiyoudoushi));
+    assert_eq!(
+        categorize(&details[1]),
+        Some(LexicalCategory::AuxiliaryVerb)
+    );
+    assert_eq!(
+        categorize(&details[2]),
+        Some(LexicalCategory::NegationDictForm)
+    );
+}
 
 #[tokio::test]
 async fn 難しくなかった() {
@@ -424,7 +507,7 @@ async fn きれいで() {
     assert_eq!(categorize(&details[0]), Some(LexicalCategory::Keiyoudoushi));
     assert_eq!(
         categorize(&details[1]),
-        Some(LexicalCategory::ContinuativeParticle)
+        Some(LexicalCategory::ContinuativeAuxiliary)
     );
 }
 
@@ -462,6 +545,6 @@ async fn うちにいる() {
     let details = tokenize("うちにいる").await.unwrap();
     assert_eq!(details.len(), 3);
     assert_eq!(categorize(&details[0]), Some(LexicalCategory::Noun));
-    assert_eq!(categorize(&details[1]), Some(LexicalCategory::Particle));
+    assert_eq!(categorize(&details[1]), Some(LexicalCategory::CaseParticle));
     assert_eq!(categorize(&details[2]), Some(LexicalCategory::Verb));
 }
